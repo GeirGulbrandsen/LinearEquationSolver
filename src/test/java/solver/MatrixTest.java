6aed2287a8d60138234a09b94aa917d1328eee0b
GeirGulbrandsen <dobbelg@gmail.com>
2019-08-29 14:23:17 +0100

    Moved tests to where they belong

--- src/test/java/solver/MatrixTest.java
+++ src/test/java/solver/MatrixTest.java
@@ -4,36 +4,9 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
-import static solver.Solver.gaussJordanElim;
 import static solver.Matrix.readMatrixFromFile;
 
 public class MatrixTest {
-
-    @Test
-    public void weCanPerformGaussJordanEliminationOnAThreeByFourMatrix() {
-        Matrix matrix = new Matrix(new Row[]{
-                new Row("R1", new double[]{1, 1, 2, 9}),
-                new Row("R2", new double[]{2, 4, -3, 1}),
-                new Row("R3", new double[]{3, 6, -5, 0})});
-
-        gaussJordanElim(matrix);
-
-        assertArrayEquals(new double[]{1.0, 0.0, 0.0, 1.0}, matrix.rows[0].getCoefficients(), 0.001);
-        assertArrayEquals(new double[]{0.0, 1.0, 0.0, 2.0}, matrix.rows[1].getCoefficients(), 0.001);
-        assertArrayEquals(new double[]{0.0, 0.0, 1.0, 3.0}, matrix.rows[2].getCoefficients(), 0.001);
-    }
-
-    @Test
-    public void weCanPerformGaussJordanEliminationWhenWeNeedToNormaliseTheFirstRow() {
-        Matrix matrix = new Matrix(new Row[]{
-                new Row("R1", new double[]{2, 0, 2}),
-                new Row("R2", new double[]{0, 2, 2})});
-
-        gaussJordanElim(matrix);
-
-        assertArrayEquals(new double[]{1.0, 0.0, 1.0}, matrix.rows[0].getCoefficients(), 0.001);
-        assertArrayEquals(new double[]{0.0, 1.0, 1.0}, matrix.rows[1].getCoefficients(), 0.001);
-    }
 
     @Test
     public void readMatrixFromFileTest() {
